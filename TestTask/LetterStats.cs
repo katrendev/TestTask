37dbed3ca952a40cc5a3ad3cf74e7228e7bfaf3e
KatrenZibarev <37619621+KatrenZibarev@users.noreply.github.com>
2018-03-21 18:21:44 +0700

    Базовые классы и методы для последующей реализации.

---
+++ TestTask/LetterStats.cs
@@ -0,0 +1,18 @@
+﻿namespace TestTask
+{
+    /// <summary>
+    /// Статистика вхождения буквы/пары букв
+    /// </summary>
+    public struct LetterStats
+    {
+        /// <summary>
+        /// Буква/Пара букв для учёта статистики.
+        /// </summary>
+        public string Letter;
+
+        /// <summary>
+        /// Кол-во вхождений буквы/пары.
+        /// </summary>
+        public int Count;
+    }
+}
