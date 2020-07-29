fb027687cb1488b228c6b3ccbaaf538352f6d296
Inga24 <68746834+Inga24@users.noreply.github.com>
2020-07-29 12:04:00 +0700

    Update IReadOnlyStream.cs

--- TestTask/IReadOnlyStream.cs
+++ TestTask/IReadOnlyStream.cs
@@ -1,10 +1,11 @@
-﻿namespace TestTask
+using System;
+namespace TestTask
 {
     /// <summary>
     /// Интерфейс для работы с файлом в сильно урезаном виде.
     /// Умеет всего 2 вещи: прочитать символ, и перемотать стрим на начало.
     /// </summary>
-    internal interface IReadOnlyStream
+    internal interface IReadOnlyStream : IDisposable
     {
         // TODO : Необходимо доработать данный интерфейс для обеспечения гарантированного закрытия файла, по окончанию работы с таковым!
         char ReadNextChar();
@@ -12,5 +13,7 @@
         void ResetPositionToStart();
 
         bool IsEof { get; }
+        
+        void Close();
     }
 }
