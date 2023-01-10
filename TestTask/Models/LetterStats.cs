f2ddd072709d89d97b5fb3a24d1d4d91e2d0e98a
NiceManItIsAboutMe <oookosta81@gmail.com>
2023-01-10 13:57:14 +0700

    Изменение модификатора доступа к полю Count

--- TestTask/Models/LetterStats.cs
+++ TestTask/Models/LetterStats.cs
@@ -8,12 +8,12 @@
         /// <summary>
         /// Буква/Пара букв для учёта статистики.
         /// </summary>
-        public string Letter;
+        public string Letter { get; set; }
 
         /// <summary>
         /// Кол-во вхождений буквы/пары.
         /// </summary>
-        public int Count;
+        public int Count { get; private set; }
 
         /// <summary>
         /// Метод увеличивает счётчик вхождений.
