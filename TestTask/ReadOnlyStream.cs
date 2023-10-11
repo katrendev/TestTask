fc7e67fcd6a6bbfacb9577144f3ecadd3691cc24
MAIN-PC\Denis <dennis.borodulkin@mail.ru>
2023-10-11 16:49:13 +0700

    feat: создан стрим для чтения файла

--- TestTask/ReadOnlyStream.cs
+++ TestTask/ReadOnlyStream.cs
@@ -15,12 +15,10 @@ namespace TestTask
         /// <param name="fileFullPath">Полный путь до файла для чтения</param>
         public ReadOnlyStream(string fileFullPath)
         {
-            IsEof = true;
-
             // TODO : Заменить на создание реального стрима для чтения файла!
-            _localStream = null;
+            _localStream = new FileStream(fileFullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
         }
-                
+
         /// <summary>
         /// Флаг окончания файла.
         /// </summary>
