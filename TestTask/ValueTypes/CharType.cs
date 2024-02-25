af45fde366afbc7455fa55ce6d8142c8cdc3befd
m.muravev <m.muravev@collegeschedule.ru>
2024-02-25 10:39:00 +0300

    feat(values): moved to a separate namespace

---
+++ TestTask/ValueTypes/CharType.cs
@@ -0,0 +1,18 @@
+﻿namespace TestTask
+{
+    /// <summary>
+    /// Тип букв
+    /// </summary>
+    public enum CharType
+    {
+        /// <summary>
+        /// Гласные
+        /// </summary>
+        Vowel,
+
+        /// <summary>
+        /// Согласные
+        /// </summary>
+        Consonants
+    }
+}
