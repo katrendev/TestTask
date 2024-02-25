1baa5d2caf52d0e3b33a4e3d32b035d19b16908f
m.muravev <m.muravev@collegeschedule.ru>
2024-02-25 10:38:10 +0300

    feat(extensions): added extensions for chars and collections

---
+++ TestTask/Extensions/CharExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace TestTask.Extension
+{
+    public static class CharExtensions
+    {
+        /// <summary>
+        /// Расширение для типа данных char
+        /// </summary>
+        /// <returns>Результат, является ли символ гласной буквой</returns>
+        /// <param name="c">Символ, который необходимо проверить</param>
+        public static bool IsVowel(this char c)
+        {
+            return "AEIOUАЕЁИОУЫЭЮЯ".Contains(char.ToUpper(c));
+        }
+
+        /// <summary>
+        /// Расширение для типа данных char
+        /// </summary>
+        /// <returns>Результат, является ли символ согласной буквой</returns>
+        /// <param name="c">Символ, который необходимо проверить</param>
+        public static bool IsConsonant(this char c)
+        {
+            return char.IsLetter(c) && !IsVowel(c);
+        }
+    }
+}
---
+++ TestTask/Extensions/CollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask.Extension
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Расширение для типа данных ICollection(IList)
+        /// </summary>
+        /// <returns>Коллекция без элементов, удаленных по условию</returns>
+        /// <param name="this">Коллекция, с которой проводится работа</param>
+        /// <param name="predicate">Условие удаления элементов из коллекции</param>
+        public static void RemoveAll<T>(this ICollection<T> @this, Func<T, bool> predicate)
+        {
+            if (@this is List<T> list)
+            {
+                list.RemoveAll(new Predicate<T>(predicate));
+            }
+            else
+            {
+                List<T> itemsToDelete = @this
+                    .Where(predicate)
+                    .ToList();
+
+                foreach (var item in itemsToDelete)
+                {
+                    @this.Remove(item);
+                }
+            }
+        }
+    }
+}
